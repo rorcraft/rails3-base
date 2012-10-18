c4711c50c09fd82b83b086948c6849f0fe7be92f
Timur Batyrshin <timur.batyrshin@flathost.ru>
2012-10-18 20:16:59 +0400

    changed AMI to be EBS-backed

--- config/chef/knife.rb
+++ config/chef/knife.rb
@@ -2,4 +2,4 @@ validation_key '/dev/null'
 chef_server_url ''
 knife[:aws_access_key_id]     = "#{ENV['AWS_ACCESS_KEY_ID']}"
 knife[:aws_secret_access_key] = "#{ENV['AWS_SECRET_ACCESS_KEY']}"
-knife[:image]                 = 'ami-834cf1ea'
+knife[:image]                 = 'ami-3d4ff254'
