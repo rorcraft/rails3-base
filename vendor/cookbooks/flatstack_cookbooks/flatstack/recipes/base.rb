3650ec7f431d6488849f7b3d2f43e9443137622f
Timur Batyrshin <timur.batyrshin@flathost.ru>
2012-10-18 20:12:18 +0400

    added a few useful tweaks to the server

--- vendor/cookbooks/flatstack_cookbooks/flatstack/recipes/base.rb
+++ vendor/cookbooks/flatstack_cookbooks/flatstack/recipes/base.rb
@@ -2,6 +2,12 @@ include_recipe 'apt'
 include_recipe 'hostname'
 include_recipe 'vim'
 include_recipe 'git'
+include_recipe 'htop'
+include_recipe 'motd-tail'
+
+magic_shell_alias "l" do
+  command "ls -l"
+end
 
 package 'nano' do
   action :remove
