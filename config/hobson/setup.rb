ac59f692f12f9642699eb2abcaf98bfa75b2ab34
Rex Chung <tech_ops+rex@change.org>
2013-01-29 13:29:46 -0800

    hobson setup db

--- config/hobson/setup.rb
+++ config/hobson/setup.rb
@@ -5,3 +5,4 @@ ENV['HOBSON_SETUP_HOOK_RUN'] = "true"
 Hobson.logger.info "copy database.yml"
 
 execute "cp config/database.yml.example config/database.yml"
+execute "RAILS_ENV=test rake db:create db:migrate"
